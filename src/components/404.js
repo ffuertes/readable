f216c99c7f691494fbc56c64dcbb83533b565982
ffuertes <felipe.fuertes@gmail.com>
2018-01-11 14:46:14 -0400

    feat: Handle Not found/Not matches URLs

---
+++ src/components/404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+	return (
+		<div className="not-found">
+			<h2>We are very sorry...</h2>
+			The page you were looking for slipped through a crack and can't be found!.
+		</div>
+	);
+}
+
+export default NotFound;
--- src/components/App.js
+++ src/components/App.js
@@ -12,10 +12,21 @@ import PostDetail from './Post/PostDetail';
 import Category from './Category/Category';
 import CreatePost from './Post/CreatePost';
 import EditPost from './Post/EditPost';
+import NotFound from './404';
 
 class App extends Component {
   componentDidMount() {
     this.props.fetchData();
+  }
+
+  categoryExists = category => {
+    const categories = ['react', 'redux', 'udacity'];
+    return categories.indexOf(category) > -1;
+  }
+
+  postExists = (category, post) => {
+    const {posts} = this.props;
+    return posts.hasOwnProperty(post) && posts[post].category === category;
   }
 
   render() {
@@ -26,14 +37,28 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/add-post" exact component={CreatePost} />
-            <Route path="/:category/:postId/edit" exact component={EditPost} />
-            <Route path="/:category/:postId" exact component={PostDetail} />
-            <Route path="/:catId" exact component={Category} />
+            <Route path="/:category/:postId/edit" exact render={ props => {
+              const {postId, category} = props.match.params;
+              return this.postExists(category, postId) ? <EditPost {...props} /> : <NotFound />
+            }} />
+            <Route path="/:category/:postId" exact render={ props => {
+              const {postId, category} = props.match.params;
+              return this.postExists(category, postId) ? <PostDetail {...props} /> : <NotFound />
+            }} />
+            <Route path="/:catId" exact render={ props => {
+              const {catId} = props.match.params;
+              return this.categoryExists(catId) ? <Category {...props} /> : <NotFound />
+            }} />
+            <Route component={NotFound} />
           </Switch>
         </div>
       </Router>
     );
   }
+}
+
+const mapStateToProps = ({posts}) => {
+	return {posts}
 }
 
 const mapDispatchToProps = dispatch => {
@@ -42,4 +67,4 @@ const mapDispatchToProps = dispatch => {
 	}
 }
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
